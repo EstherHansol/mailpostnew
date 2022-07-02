becabe0affe85bdf90d4aa50ca9a0e7da692085a
khw18 <gmsb14@naver.com>
2022-07-02 16:16:17 +0900

    편지 작성 데이터 이동 구현

--- app/src/main/java/com/example/mailpost/ui/MainActivity.kt
+++ app/src/main/java/com/example/mailpost/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mailpost.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
@@ -9,6 +10,7 @@ import com.example.mailpost.R
 import com.example.mailpost.databinding.ActivityMainBinding
 import com.example.mailpost.ui.home.HomeFragment
 import com.example.mailpost.ui.letterbox.LetterBoxFragment
+import com.example.mailpost.ui.utils.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
@@ -24,6 +26,41 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             navView.setOnNavigationItemSelectedListener(this@MainActivity)
             supportFragmentManager.beginTransaction().add(R.id.nav_host_fragment, HomeFragment())
                 .commitAllowingStateLoss()
+            writeBtn.setOnClickListener{
+                intent = Intent(this@MainActivity, WriteActivity::class.java)
+                startActivity(intent)
+            }
+            val title = intent.getStringExtra("title")
+            val content = intent.getStringExtra("content")
+            val from = intent.getStringExtra("from")
+            val to = intent.getStringExtra("to")
+
+
+
+            if(title != null && content != null && from != null && to != null) {
+                if (pub) {
+                    titleList[cnt] = title
+                    contentList[cnt] = content
+                    fromList[cnt] = from
+                    toList[cnt] = to
+
+                    ptitleList[pubcnt] = title
+                    pcontentList[pubcnt] = content
+                    pfromList[pubcnt] = from
+                    ptoList[pubcnt] = to
+
+                    pub = false
+                }
+                else
+                {
+                    titleList[cnt] = title
+                    contentList[cnt] = content
+                    fromList[cnt] = from
+                    toList[cnt] = to
+                }
+            }
+
+
         }
 
     }
@@ -31,6 +68,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.navigation_home -> {
+
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.nav_host_fragment, HomeFragment()).commitAllowingStateLoss()
             }
--- app/src/main/java/com/example/mailpost/ui/utils/DummyProvider.kt
+++ app/src/main/java/com/example/mailpost/ui/utils/DummyProvider.kt
@@ -3,7 +3,27 @@ package com.example.mailpost.ui.utils
 import com.example.mailpost.model.Comment
 import com.example.mailpost.model.Letter
 
+    var cnt = -1
+    var pubcnt = -1
+    var pub = false
+
+    var titleList = Array(10,{""})
+    var contentList = Array(10,{""})
+    var fromList = Array(10,{""})
+    var toList = Array(10,{""})
+
+
+    //public
+    var ptitleList = Array(10,{""})
+    var pcontentList = Array(10,{""})
+    var pfromList = Array(10,{""})
+    var ptoList = Array(10,{""})
+
+
+
+//비공개편지
 fun providePrivateLetters(): List<Letter> {
+
     val letterList = mutableListOf<Letter>()
 
     val firstDummyLetter = Letter(
@@ -33,6 +53,60 @@ fun providePrivateLetters(): List<Letter> {
     )
 
     letterList.addAll(listOf(firstDummyLetter, secondDummyLetter, thirdDummyLetter))
+
+
+
+    if(cnt != -1 && titleList[cnt].isNotEmpty()) {
+        for(i in 0 .. cnt) {
+            letterList.add(Letter(titleList[i], contentList[i], fromList[i], toList[i]))
+        }
+    }
+
+    return letterList
+}
+
+
+
+//공개편지
+fun providePublicLetters(): List<Letter> {
+
+    val letterList = mutableListOf<Letter>()
+
+    val firstDummyLetter = Letter(
+        "뽀삐야, 보고싶어.",
+        "하늘 나라에서도 잘 지내고 있는거지?",
+        "민지",
+        "뽀삐",
+        listOf(Comment(0, 0, "", System.currentTimeMillis()),
+            Comment(0, 0, "", System.currentTimeMillis()),
+            Comment(0, 0, "", System.currentTimeMillis()))
+    )
+
+    val secondDummyLetter = Letter(
+        "내일의 나에게.",
+        "내일도 편지 한 통 쓰자!",
+        "민지",
+        "스스로",
+    )
+
+    val thirdDummyLetter = Letter(
+        "성남 초등학교 오성태!!",
+        "어릴 때 같이 놀이터에서 많이 놀았었는데 요즘엔 뭐하는지 궁금하네. 글 보면 연락 줘!",
+        "민지",
+        "성태",
+        listOf(Comment(0, 0, "", System.currentTimeMillis()),
+            Comment(0, 0, "", System.currentTimeMillis()))
+    )
+
+    letterList.addAll(listOf(firstDummyLetter, secondDummyLetter, thirdDummyLetter))
+
+
+
+    if(pubcnt != -1 && ptitleList[pubcnt].isNotEmpty()) {
+        for(i in 0 .. pubcnt) {
+            letterList.add(Letter(ptitleList[i], pcontentList[i], pfromList[i], ptoList[i]))
+        }
+    }
 
     return letterList
 }
