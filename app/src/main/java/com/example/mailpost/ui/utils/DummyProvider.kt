97dfe822af50be2cde3321100bc178f2a485ccb9
khw18 <gmsb14@naver.com>
2022-07-02 18:20:16 +0900

    홈 프래그먼트 더미 데이터추가

--- app/src/main/java/com/example/mailpost/ui/utils/DummyProvider.kt
+++ app/src/main/java/com/example/mailpost/ui/utils/DummyProvider.kt
@@ -72,7 +72,7 @@ fun providePublicLetters(): List<Letter> {
 
     val letterList = mutableListOf<Letter>()
 
-    val firstDummyLetter = Letter(
+    val DummyLetter1 = Letter(
         "뽀삐야, 보고싶어.",
         "하늘 나라에서도 잘 지내고 있는거지?",
         "민지",
@@ -82,14 +82,33 @@ fun providePublicLetters(): List<Letter> {
             Comment(0, 0, "", System.currentTimeMillis()))
     )
 
-    val secondDummyLetter = Letter(
+    val DummyLetter2 = Letter(
+        "김병장님",
+        "전역하셨으니 하는 말인데 그리 살지 마십쇼",
+        "곽철우",
+        "김찬호",
+        listOf(Comment(0, 0, "", System.currentTimeMillis()),
+            Comment(0, 0, "", System.currentTimeMillis()))
+    )
+
+    val DummyLetter3 = Letter(
         "내일의 나에게.",
         "내일도 편지 한 통 쓰자!",
         "민지",
         "스스로",
     )
 
-    val thirdDummyLetter = Letter(
+    val DummyLetter4 = Letter(
+        "먼저간남편께",
+        "왜이러케빨리갓소보고싶어요",
+        "김춘자",
+        "김판호",
+        listOf(Comment(0, 0, "", System.currentTimeMillis()),
+            Comment(0, 0, "", System.currentTimeMillis()),
+            Comment(0, 0, "", System.currentTimeMillis()))
+    )
+
+    val DummyLetter5 = Letter(
         "성남 초등학교 오성태!!",
         "어릴 때 같이 놀이터에서 많이 놀았었는데 요즘엔 뭐하는지 궁금하네. 글 보면 연락 줘!",
         "민지",
@@ -98,7 +117,38 @@ fun providePublicLetters(): List<Letter> {
             Comment(0, 0, "", System.currentTimeMillis()))
     )
 
-    letterList.addAll(listOf(firstDummyLetter, secondDummyLetter, thirdDummyLetter))
+    val DummyLetter6 = Letter(
+        "I miss you, John",
+        "I'm sad that we can't meet because *ucking COVID-19. ",
+        "제시카 초이",
+        "존 존스",
+        listOf(Comment(0, 0, "", System.currentTimeMillis()),
+                Comment(0, 0, "", System.currentTimeMillis()),
+            Comment(0, 0, "", System.currentTimeMillis()))
+    )
+
+    val DummyLetter7 = Letter(
+        "도라에몽 잘 지내니?",
+        "나 다음 달에 이슬이랑 결혼해. 니가 왔으면 좋겠는데..",
+        "노진구",
+        "도라에몽",
+        listOf(Comment(0, 0, "", System.currentTimeMillis()),
+            Comment(0, 0, "", System.currentTimeMillis()))
+    )
+    val DummyLetter8 = Letter(
+        "티라노사우르스야 아쉽구나",
+        "몇 억년만 일찍 태어나면 우리 만날 수 있었을 텐데.. ",
+        "김디아나존스",
+        "티라노사우르스",
+        listOf(Comment(0, 0, "", System.currentTimeMillis()),
+            Comment(0, 0, "", System.currentTimeMillis()),
+            Comment(0, 0, "", System.currentTimeMillis()),
+            Comment(0, 0, "", System.currentTimeMillis()),
+            Comment(0, 0, "", System.currentTimeMillis()))
+    )
+
+
+    letterList.addAll(listOf(DummyLetter1, DummyLetter2, DummyLetter3, DummyLetter4, DummyLetter5, DummyLetter6, DummyLetter7, DummyLetter8))
 
 
 
